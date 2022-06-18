8f26b724522110c9285bb454701dddbb23b42045
= <=>
2022-06-18 20:32:09 +0900

    リソースのみを含むJarを生成できるように変更

--- app/src/main/java/test/Main.java
+++ app/src/main/java/test/Main.java
@@ -1,9 +1,12 @@
 package test;
 
+import org.teavm.jso.browser.Window;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception
 	{
+		Window.current();
 		System.out.println("HelloWorld");
 	}
 
