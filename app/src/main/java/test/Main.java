ab75324c1ad4d8eeef80bb08aea12cebabef94d7
yu64 <yumaikuto1212@gmail.com>
2022-06-20 13:46:06 +0900

    依存関係を除外

--- app/src/main/java/test/Main.java
+++ app/src/main/java/test/Main.java
@@ -1,11 +1,18 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println("HelloWorld");
+		InputStream is = Main.class.getClassLoader().getResourceAsStream("test/resource/app.js");
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		
+		br.lines().forEach(System.out::println);
 	}
 
 
